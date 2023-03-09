76937cb269b2c93b13f2d5406357114faf4d2b92
Gashaye Adugna <gasha.como@gmail.com>
2023-03-08 23:57:48 -0800

    x

--- javascrip/index.js
+++ javascrip/index.js
@@ -1,3 +1,11 @@
 function msg(){  
     alert("Hello Gashaye");  
    }  
+function printvalue(){  
+var name=document.form1.name.value;  
+alert("Welcome: "+name);  
+}  
+function getcube(){  
+    var number=document.getElementById("number").value;  
+    alert(number*number*number);  
+    }  
